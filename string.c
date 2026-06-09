aa0a3989ec9b7e1d8ec15ee1fa623c80c2081e95
Michael Cromer <mcromer@proton.me>
2026-06-09 16:01:00 +1000

    Add strstr and substring functions

--- string.c
+++ string.c
@@ -1,12 +1,13 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 struct String
 {
     size_t len;
     size_t max;
-    char *str;
+    char *data;
 };
 
 
@@ -14,7 +15,7 @@ void string_clear(struct String *string)
 {
     if (!string) return;
 
-    string->str[0] = 0;
+    string->data[0] = 0;
     string->len = 0;
 }
 
@@ -23,7 +24,7 @@ void string_destroy(struct String *string)
 {
     if (!string) return;
 
-    free(string->str);
+    free(string->data);
     free(string);
 }
 
@@ -32,13 +33,13 @@ size_t string_realloc(struct String *string, size_t new)
 {
     if (!string) return 0;
 
-    char *tmp = realloc(string->str, new + 1);
+    char *tmp = realloc(string->data, new + 1);
     if (!tmp) return 0;
 
-    string->str = tmp;
+    string->data = tmp;
     string->max = new;
     if (string->len > string->max) string->len = string->max;
-    string->str[string->len] = 0;
+    string->data[string->len] = 0;
 
     return new;
 }
@@ -49,8 +50,8 @@ struct String *string_create(size_t max)
     struct String *string = malloc(sizeof(struct String));
     if (!string) return NULL;
     
-    string->str = calloc((max + 1), sizeof(char));
-    if (!string->str) {
+    string->data = calloc((max + 1), sizeof(char));
+    if (!string->data) {
         string_destroy(string);
         return NULL;
     }
@@ -81,7 +82,25 @@ const char *string_str(const struct String *string)
 {
     if (!string) return 0;
 
-    return (const char *)string->str;
+    return (const char *)string->data;
+}
+
+
+int string_strcmp(const struct String *string, const char *str)
+{
+    return strcmp((!string) ? NULL : string->data, str);
+}
+
+
+int string_strncmp(const struct String *string, const char *str, size_t n)
+{
+    return strncmp((!string) ? NULL : string->data, str, n);
+}
+
+
+int string_compare(const struct String *string1, const struct String *string2)
+{
+    return string_strcmp(string1, (!string2) ? NULL : string2->data);
 }
 
 
@@ -91,9 +110,9 @@ void string_strcpy(struct String *dst, const char *src)
     if (!dst || !src) return;
 
     size_t i = 0;
-    for (i = 0; (i < dst->max) && (src[i]); i++) dst->str[i] = src[i];
+    for (i = 0; (i < dst->max) && (src[i]); i++) dst->data[i] = src[i];
     dst->len = i;
-    dst->str[i] = 0;
+    dst->data[i] = 0;
 }
 
 
@@ -102,8 +121,8 @@ void string_strncpy(struct String *dst, const char *src, size_t n)
     if (!dst || !src) return;
     
     size_t len_cpy = (n > dst->max) ? dst->max : n;
-    for (size_t i = 0; i < len_cpy; i++) dst->str[i] = src[i];
-    dst->str[len_cpy] = 0;
+    for (size_t i = 0; i < len_cpy; i++) dst->data[i] = src[i];
+    dst->data[len_cpy] = 0;
     dst->len = len_cpy;
 }
 
@@ -116,7 +135,20 @@ void string_copy(struct String *dst, const struct String *src)
         if (!string_realloc(dst, src->len)) return;
     }
 
-    string_strncpy(dst, src->str, src->len);
+    string_strncpy(dst, src->data, src->len);
+}
+
+
+struct String *string_strdup(const char *src)
+{
+    if (!src) return NULL;
+    size_t n = strlen(src);
+
+    struct String *dst = string_create(n);
+    if (!dst) return NULL;
+
+    string_strncpy(dst, src, n);
+    return dst;
 }
 
 
@@ -136,7 +168,7 @@ struct String *string_duplicate(const struct String *src)
 {
     if (!src) return NULL;
 
-    return string_strndup(src->str, src->len);
+    return string_strndup(src->data, src->len);
 }
 
 
@@ -145,10 +177,10 @@ void string_strncat(struct String *dst, const char *src, size_t n)
     if (!dst || !src) return;
 
     size_t len_cat = (dst->len + n > dst->max) ? (dst->max - dst->len) : n;
-    for (size_t i = 0; i < len_cat; i++) dst->str[dst->len + i] = src[i];
+    for (size_t i = 0; i < len_cat; i++) dst->data[dst->len + i] = src[i];
 
     dst->len += len_cat;
-    dst->str[dst->len] = 0;
+    dst->data[dst->len] = 0;
 }
 
 
@@ -160,5 +192,17 @@ void string_catenate(struct String *dst, const struct String *src)
         if (!string_realloc(dst, dst->len + src->len)) return;
     }
 
-    string_strncat(dst, src->str, src->len);
+    string_strncat(dst, src->data, src->len);
+}
+
+
+const char *string_strstr(const struct String *haystack, const char *needle)
+{
+    return strstr((!haystack) ? NULL : haystack->data, needle);
+}
+
+
+const char *string_substring(const struct String *haystack, const struct String *needle)
+{
+    return string_strstr(haystack, (!needle) ? NULL : needle->data);
 }
--- string.h
+++ string.h
@@ -11,20 +11,26 @@ size_t         string_realloc(struct String *string, size_t new);
 void           string_destroy(struct String *string);
 
 /* access */
-size_t      string_len(const struct String *string);
-size_t      string_max(const struct String *string);
-const char *string_str(const struct String *string);
+size_t         string_len(const struct String *string);
+size_t         string_max(const struct String *string);
+const char    *string_str(const struct String *string);
 
 /* primitive ops: char * source */
+const char    *string_strstr(const struct String *haystack, const char *needle);
+int            string_strcmp(const struct String *string, const char *str);
+int            string_strncmp(const struct String *string, const char *str, size_t n);
+struct String *string_strdup(const char *src);
 struct String *string_strndup(const char *src, size_t n);
 void           string_strcpy(struct String *dst, const char *src);
 void           string_strncat(struct String *dst, const char *src, size_t n);
 void           string_strncpy(struct String *dst, const char *src, size_t n);
 
 /* string ops: String * source */
+const char    *string_substring(const struct String *haystack, const struct String *needle);
+int            string_compare(const struct String *string1, const struct String *string2);
 struct String *string_duplicate(const struct String *src);
-void           string_clear(struct String *string);
 void           string_catenate(struct String *dst, const struct String *src);
+void           string_clear(struct String *string);
 void           string_copy(struct String *dst, const struct String *src);
 
 #endif
